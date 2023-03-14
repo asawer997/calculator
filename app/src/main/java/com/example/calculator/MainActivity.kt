8315f9d7d005c709d46c599c23035038d99d8219
أســـــاور <xyzas.7510@gmail.com>
2023-03-14 08:58:45 +0300

    edit logic

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -18,51 +18,109 @@ class MainActivity : AppCompatActivity() {
     fun onBinaryConvertButtonClick(view: View) {
         val inputNumber = binding.numberInputEditText.text.toString()
 
-        // Convert to binary
-        val binaryResult = Integer.toBinaryString(inputNumber.toInt())
-        binding.resultTextView.text = "Binary: $binaryResult"
+        when{
+            inputNumber.matches("[0-7]+".toRegex()) -> {
+                // octal to binary conversion
+                val decimal = Integer.parseInt(inputNumber, 8)
+                binding.resultTextView.text = "Binary: ${Integer.toBinaryString(decimal)}"
+            }
+            inputNumber.matches("[01]+".toRegex()) -> {
+                // already binary
+                binding.resultTextView.text = "Binary: $inputNumber"
+            }
+            inputNumber.matches("[0-9]+".toRegex()) -> {
+                // decimal to binary conversion
+                val decimal = Integer.parseInt(inputNumber, 10)
+                binding.resultTextView.text = "Binary: ${Integer.toBinaryString(decimal)}"
+            }
+            else -> {
+                // hex to binary conversion
+                val decimal = Integer.parseInt(inputNumber, 16)
+                binding.resultTextView.text = "Binary: ${Integer.toBinaryString(decimal)}"
+            }
+        }
     }
 
     fun onDecimalConvertButtonClick(view: View) {
         val inputNumber = binding.numberInputEditText.text.toString()
 
-        // Display input as decimal
-        binding.resultTextView.text = "Decimal: $inputNumber"
+        when{
+            inputNumber.matches("[0-7]+".toRegex()) -> {
+                // octal to decimal conversion
+                val decimal = Integer.parseInt(inputNumber, 8)
+                binding.resultTextView.text = "Decimal: $decimal"
+            }
+            inputNumber.matches("[01]+".toRegex()) -> {
+                // binary to decimal conversion
+                val decimal = Integer.parseInt(inputNumber, 2)
+                binding.resultTextView.text = "Decimal: $decimal"
+            }
+            inputNumber.matches("[0-9A-Fa-f]+".toRegex()) -> {
+                // hexadecimal to decimal conversion
+                val decimal = Integer.parseInt(inputNumber, 16)
+                binding.resultTextView.text = "Decimal: $decimal"
+            }
+            else -> {
+                // already decimal
+                binding.resultTextView.text = "Decimal: $inputNumber"
+            }
+        }
     }
 
     fun onHexaConvertButtonClick(view: View) {
         val inputNumber = binding.numberInputEditText.text.toString()
-
-        // Convert to hexadecimal
-        val hexadecimalResult = Integer.toHexString(inputNumber.toInt()).toUpperCase()
-        binding.resultTextView.text = "Hexadecimal: $hexadecimalResult"
+        when{
+            inputNumber.matches("[0-7]+".toRegex()) -> {
+                // octal to hex conversion
+                val decimal = Integer.parseInt(inputNumber, 8)
+                binding.resultTextView.text = "Hexadecimal: ${Integer.toHexString(decimal).uppercase()}"
+            }
+            inputNumber.matches("[01]+".toRegex()) -> {
+                // binary to hex conversion
+                val decimal = Integer.parseInt(inputNumber, 2)
+                binding.resultTextView.text = "Hexadecimal: ${Integer.toHexString(decimal).uppercase()}"
+            }
+            inputNumber.matches("[0-9]+".toRegex()) -> {
+                // decimal to hex conversion
+                val decimal = Integer.parseInt(inputNumber, 10)
+                binding.resultTextView.text = "Hexadecimal: ${Integer.toHexString(decimal).uppercase()}"
+            }
+            else -> {
+                // already hex
+                binding.resultTextView.text = "Hexadecimal: ${inputNumber.uppercase()}"
+            }
+        }
     }
 
     fun onOctalConvertButtonClick(view: View) {
         val inputNumber = binding.numberInputEditText.text.toString()
-        val octalResult = Integer.toOctalString(inputNumber.toInt())
-        binding.resultTextView.text = "Octal: $octalResult"
+        when {
+            inputNumber.matches("[0-7]+".toRegex()) -> {
+                // already octal
+                binding.resultTextView.text = "Octal: $inputNumber"
+            }
+            inputNumber.matches("[01]+".toRegex()) -> {
+                // binary to octal conversion
+                val decimal = Integer.parseInt(inputNumber, 2)
+                binding.resultTextView.text = "Octal: ${Integer.toOctalString(decimal)}"
+            }
+            inputNumber.matches("[0-9]+".toRegex()) -> {
+                // decimal to octal conversion
+                val decimal = Integer.parseInt(inputNumber, 10)
+                binding.resultTextView.text = "Octal: ${Integer.toOctalString(decimal)}"
+            }
+            else -> {
+                // hex to octal conversion
+                val decimal = Integer.parseInt(inputNumber, 16)
+                binding.resultTextView.text = "Octal: ${Integer.toOctalString(decimal)}"
+            }
+        }
     }
 
     fun onClearButtonClick(view: View) {
         binding.numberInputEditText.text.clear()
         binding.resultTextView.text = ""
     }
-
-//    fun determineInputFormat(view: View) {
-//        val inputNumber = binding.numberInputEditText.text.toString()
-//        val decimalRegex = "^-?[0-9]+$".toRegex()
-//        val hexRegex = "^-?0[xX][0-9a-fA-F]+$".toRegex()
-//        val binaryRegex = "^-?[01]+$".toRegex()
-//        val octalRegex = "^-?[0-7]+$".toRegex()
-//        when {
-//            inputNumber.matches(decimalRegex) -> onDecimalButtonClick(view,inputNumber)
-//            inputNumber.matches(hexRegex) -> onHexadecimalButtonClick(view,inputNumber)
-//            inputNumber.matches(binaryRegex) -> onBinaryButtonClick(view,inputNumber)
-//            inputNumber.matches(octalRegex) -> onOctalButtonClick(view,inputNumber)
-//            else -> "unknown"
-//        }
-//    }
 
 
 }
